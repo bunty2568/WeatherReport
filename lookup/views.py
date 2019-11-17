264a115fedf3cbb154e6ffd60971c7c1e9f9b7c5
Dev Agastya <devagastya7@gmail.com>
2019-11-18 02:12:06 +0530

    finished

--- lookup/views.py
+++ lookup/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request, 'home.html',{})
+	import json
+	import requests
+
+	if request.method =="POST":
+		zipcode= request.POST['zipcode']
+		api_request = requests.get("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=0FOy9nMuRCDqM8VT1G52yRjJeOLnYsNV&q= + zipcode + ")
+			#"http://dataservice.accuweather.com/locations/v1/cities/search?apikey=0FOy9nMuRCDqM8VT1G52yRjJeOLnYsNV&q=Bangalore"
+			#http://dataservice.accuweather.com/locations/v1/cities/search?apikey=0FOy9nMuRCDqM8VT1G52yRjJeOLnYsNV&q=Bangalore"
+		try:
+			api=json.loads(api_request.content)
+		except Exception as e:
+			api = "Error..."
+
+		return render(request, 'home.html',{ 'api' : api })
+
+
+	else:
+
+		api_request = requests.get("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=0FOy9nMuRCDqM8VT1G52yRjJeOLnYsNV&q=Bangalore")
+		#"http://dataservice.accuweather.com/locations/v1/cities/search?apikey=0FOy9nMuRCDqM8VT1G52yRjJeOLnYsNV&q=Bangalore"
+		#http://dataservice.accuweather.com/locations/v1/cities/search?apikey=0FOy9nMuRCDqM8VT1G52yRjJeOLnYsNV&q=Bangalore"
+		try:
+			api=json.loads(api_request.content)
+		except Exception as e:
+			api = "Error..."
+
+		return render(request, 'home.html',{ 'api' : api })
 
 def about(request):
 	return render(request, 'about.html',{})
